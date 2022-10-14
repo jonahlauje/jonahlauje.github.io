7a1a8f5af5f1704d57b0db6c796e7ecedd5abfff
jonahlauje <jonahlauje@hotmail.com>
2022-10-14 22:31:59 +1100

    First upload

---
+++ myWebsite/scripts/form-9d25.php
@@ -0,0 +1,30 @@
+<?php
+
+require_once('FormProcessor.php');
+
+$form = array(
+    'subject' => 'New Form Submission',
+    'email_message' => 'You have a new form submission',
+    'success_redirect' => '',
+    'sendIpAddress' => true,
+    'email' => array(
+    'from' => '',
+    'to' => ''
+    ),
+    'fields' => array(
+    'email' => array(
+    'order' => 1,
+    'type' => 'email',
+    'label' => 'Email',
+    'required' => true,
+    'errors' => array(
+    'required' => 'Field \'Email\' is required.'
+    )
+    ),
+    )
+    );
+
+    $processor = new FormProcessor('');
+    $processor->process($form);
+
+    ?>
